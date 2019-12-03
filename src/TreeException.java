29b33b3d6069d807aff271049c44676f6da502cc
OlayiwolaAnimashaun <58079895+OlayiwolaAnimashaun@users.noreply.github.com>
2019-12-03 15:40:40 +0000

    Add files via upload

---
+++ src/TreeException.java
@@ -0,0 +1,10 @@
+package huffmanCode;
+//package BinaryTrees;
+
+public class TreeException extends RuntimeException
+{
+  public TreeException(String s)
+  {
+    super(s);
+  }  // end constructor
+} // end TreeException
